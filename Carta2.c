a84c9fde8a55a9593f3a8df590a142ae84bcf35b
Joaodias05 <jovilaudias@gmail.com>
2025-02-22 18:47:07 -0300

    Add files via upload

---
+++ Carta2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main (){
+
+int populaçao = 2.817 ;
+float pib = 286.94 ;
+float area = 5.761 ;
+char estado[50] = "B";
+char codigo [50] = "A02";
+char cidade [50] = "Brasília";
+int turismo = 17;
+
+printf("Estado: %s\n Codigo: %s\n", estado, codigo);
+printf("Nome da cidade: %s\n Numero de habitantes %d.817 Milhões\n", cidade, populaçao);
+printf("Área da cidade: %2.f.761 KM\nPIB: %2.f.94 Milhões de reais\n", area, pib);
+printf("Quantidade de Pontos turísticos: %d\n", turismo);
+
+
+
+
+
+}
