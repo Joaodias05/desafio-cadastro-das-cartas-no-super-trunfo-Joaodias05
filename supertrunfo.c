4418c21355efbd069caf4a9557163630604b9656
Joaodias05 <jovilaudias@gmail.com>
2025-02-22 18:22:04 -0300

    Add files via upload

---
+++ supertrunfo.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main (){
+
+int população = 7.61;
+int turismo = 265 ;
+float pib = 505.3 ;
+float area = 95.346;
+char estado [50] = "A" ;
+char codigo [50] = "A01";
+char cidade [50] = "Florianópolis";
+
+printf("Estado: %s\n codigo: %s\n", estado, codigo);
+printf("Nome da cidade: %s\n Numero de habitantes: %d.61 Milhões de habitantes\n", cidade, população);
+printf("Área da cidade: %2.f.346 km\nPIB: %2.f.3 Bilhões de reais\n", area, pib);
+printf("Quantidade de pontos turísticos: %d\n", turismo);
+ }
+
+
+
