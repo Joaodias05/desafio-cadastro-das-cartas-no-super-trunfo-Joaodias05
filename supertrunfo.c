ee39d4687d044a8a016f286f0a8d8522cb15514e
Joao Dias <jovilaudias@gmail.com>
2025-02-25 16:56:52 -0300

    Update supertrunfo.c

--- supertrunfo.c
+++ supertrunfo.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
 int main (){
 
-int população = 7.61;
+float população = 5370000 ;
 int turismo = 265 ;
-float pib = 505.3 ;
+float pib = 23550000000 ;
 float area = 95.346;
 char estado [50] = "A" ;
 char codigo [50] = "A01";
 char cidade [50] = "Florianópolis";
-
+float densidade = população / area;
+ float capital = pib / população;
 printf("Estado: %s\n codigo: %s\n", estado, codigo);
-printf("Nome da cidade: %s\n Numero de habitantes: %d.61 Milhões de habitantes\n", cidade, população);
-printf("Área da cidade: %2.f.346 km\nPIB: %2.f.3 Bilhões de reais\n", area, pib);
+printf("Nome da cidade: %s\n Numero de habitantes: %.2f \n", cidade, população);
+printf("Área da cidade: %.2f km\nPIB: %f\n", area, pib);
 printf("Quantidade de pontos turísticos: %d\n", turismo);
- }
+printf("Densidade: %.3f Hab/Km\nCapital: %.2f de reais\n"), densidade, capital;
+
+}
 
 
 
