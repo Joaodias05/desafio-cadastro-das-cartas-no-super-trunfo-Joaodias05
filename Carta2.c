4ae504aff6f871b36703858c04e71da155d8c235
Joao Dias <jovilaudias@gmail.com>
2025-02-25 17:28:53 -0300

    Update Carta2.c

--- Carta2.c
+++ Carta2.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
 int main (){
 
-int populaçao = 2.817 ;
-float pib = 286.94 ;
-float area = 5.761 ;
+float populaçao = 2817000;
+float pib = 244000000000 ;
+float area = 5761 ;
 char estado[50] = "B";
 char codigo [50] = "A02";
 char cidade [50] = "Brasília";
 int turismo = 17;
+float Densidade = populaçao / area;
+float Capital = pib / populaçao;
 
 printf("Estado: %s\n Codigo: %s\n", estado, codigo);
-printf("Nome da cidade: %s\n Numero de habitantes %d.817 Milhões\n", cidade, populaçao);
-printf("Área da cidade: %2.f.761 KM\nPIB: %2.f.94 Milhões de reais\n", area, pib);
+printf("Nome da cidade: %s\n Numero de habitantes %2.f Milhões\n", cidade, populaçao);
+printf("Área da cidade: %.2f KM\nPIB: %.f Bilhões\n", area, pib);
 printf("Quantidade de Pontos turísticos: %d\n", turismo);
+printf("Densidade Populacional: %.2f Hab/KM\nPIB por Capital: %4.f milhões\n", Densidade, Capital);
 
 
 
